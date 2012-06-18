c5d0ae66931e9b0b436b7865288da7518d1a6673
Gage Herrmann <gage.herrmann@gmail.com>
2012-06-18 13:04:09 -0500

    Initial commit

---
+++ src/com/gagege/timekeep/Timekeep.java
@@ -0,0 +1,17 @@
+package com.gagege.timekeep;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Timekeep extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
+
+    }
+}
