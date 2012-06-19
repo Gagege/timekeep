5a96e0d9bace3f77cf2d91a54b57648b287b38ca
Gagege <gage.herrmann@gmail.com>
2012-06-19 06:50:36 -0500

    Learning more UI

--- src/com/gagege/timekeep/Timekeep.java
+++ src/com/gagege/timekeep/Timekeep.java
@@ -2,12 +2,23 @@ package com.gagege.timekeep;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 
-public class Timekeep extends Activity {
+public class Timekeep extends Activity implements OnClickListener {
+	
+    public void onClick(View v){
+    }
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ExpandableListView entryListView = (ExpandableListView)findViewById(R.id.entryLstVw);
+        entryListView.add
     }
 }
