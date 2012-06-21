f375d3d89a33f6cc86080ff2ca7c0acd6fb1fcbb
Gage Herrmann <gage.herrmann@gmail.com>
2012-06-21 13:56:44 -0500

    Changed edit screen to linear layout

--- gen/com/gagege/timekeep/R.java
+++ gen/com/gagege/timekeep/R.java
@@ -14,23 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int clientTextEdit=0x7f05000b;
-        public static final int clientTextView=0x7f05000a;
-        public static final int dateTextEdit=0x7f050002;
-        public static final int dateTextView=0x7f050001;
-        public static final int entryListView=0x7f050010;
-        public static final int hoursTextEdit=0x7f050005;
-        public static final int hoursTextView=0x7f050004;
-        public static final int itemName=0x7f05000f;
-        public static final int notesTextEdit=0x7f05000e;
-        public static final int notesTextView=0x7f05000d;
-        public static final int projectTextEdit=0x7f050008;
-        public static final int projectTextView=0x7f050007;
-        public static final int tableRow1=0x7f050000;
-        public static final int tableRow2=0x7f050003;
-        public static final int tableRow3=0x7f050006;
-        public static final int tableRow4=0x7f050009;
-        public static final int tableRow5=0x7f05000c;
+        public static final int clientTextEdit=0x7f050003;
+        public static final int dateTextEdit=0x7f050001;
+        public static final int doneButton=0x7f050005;
+        public static final int entryListView=0x7f050007;
+        public static final int hoursTextEdit=0x7f050000;
+        public static final int itemName=0x7f050006;
+        public static final int notesTextEdit=0x7f050004;
+        public static final int projectTextEdit=0x7f050002;
     }
     public static final class layout {
         public static final int edit=0x7f030000;
@@ -53,7 +44,7 @@ public final class R {
         public static final int clientTitle=0x7f04000e;
         public static final int dateTitle=0x7f04000b;
         public static final int defaultItem=0x7f04000a;
-        public static final int doneButtonText=0x7f04000f;
+        public static final int done=0x7f04000f;
         public static final int hoursTitle=0x7f04000c;
         public static final int nameLabel=0x7f040005;
         public static final int notesTitle=0x7f040011;
