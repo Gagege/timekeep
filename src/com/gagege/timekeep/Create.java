7e319c6f5605a7b1d6f7bd8dfcedc8832b865a54
Gagege <gage.herrmann@gmail.com>
2012-06-24 10:20:54 -0500

    Added date picker to create screen

--- src/com/gagege/timekeep/Create.java
+++ src/com/gagege/timekeep/Create.java
@@ -3,12 +3,13 @@ package com.gagege.timekeep;
 import java.text.DateFormat;
 import java.util.Date;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.EditText;
 
-public class Create extends Activity {
+public class Create extends FragmentActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,11 @@ public class Create extends Activity {
         EditText date = (EditText)findViewById(R.id.dateTextEdit);
         date.setText(DateFormat.getDateInstance().format(new Date()));
     }
+	
+	public void showDatePickerDialog(View view) {
+		DialogFragment newFragment = new DatePickerFragment((EditText)findViewById(R.id.dateTextEdit));
+	    newFragment.show(getSupportFragmentManager(), "datePicker");
+	}
 	
 	public void cancelClick(View view) {
 		finish();
---
+++ src/com/gagege/timekeep/DatePickerFragment.java
@@ -0,0 +1,38 @@
+package com.gagege.timekeep;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+	private EditText dateEdit;
+	
+	public DatePickerFragment(EditText textBox) {
+		super();
+		dateEdit = textBox;
+	}
+	
+	@Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+	
+		Date date = new Date(dateEdit.getText().toString());
+	    int year = date.getYear() + 1900;
+	    int month = date.getMonth();
+	    int day = date.getDate();
+	
+	    return new DatePickerDialog(getActivity(), this, year, month, day);
+	}
+	
+	public void onDateSet(DatePicker view, int year, int month, int day) {
+		Date date = new Date(year + "/" + (month + 1) + "/" + day);
+		dateEdit.setText(DateFormat.getDateInstance().format(date));
+	}
+
+}
