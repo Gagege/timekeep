9d8813944ebdad9beb05e12fd0d2f73aff80425d
Gagege <gage.herrmann@gmail.com>
2012-06-24 23:20:44 -0500

    Added focus listener to hours textbox

--- src/com/gagege/timekeep/Create.java
+++ src/com/gagege/timekeep/Create.java
@@ -7,9 +7,10 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.widget.EditText;
 
-public class Create extends FragmentActivity {
+public class Create extends FragmentActivity{
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,22 @@ public class Create extends FragmentActivity {
         
         EditText date = (EditText)findViewById(R.id.dateTextEdit);
         date.setText(DateFormat.getDateInstance().format(new Date()));
+        
+        EditText hours = (EditText)findViewById(R.id.hoursTextEdit);
+        OnFocusChangeListener focusListener = new OnFocusChangeListener() {
+            public void onFocusChange(View view, boolean hasFocus) {
+            	if(hasFocus)
+				{
+					
+				}
+				else
+				{
+					EditText hoursView = (EditText) view;
+					hoursView.setText(hoursView.getText().append(" hrs"));
+				}
+            }
+        };
+        hours.setOnFocusChangeListener(focusListener);
     }
 	
 	public void showDatePickerDialog(View view) {
